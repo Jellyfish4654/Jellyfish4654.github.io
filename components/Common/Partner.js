d1e7e9fe0c4848ed8a182446e8fe832a6bdc1d40
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2023-12-22 09:27:44 -0500

    update arrays

--- components/Common/Partner.js
+++ components/Common/Partner.js
@@ -18,31 +18,38 @@ const partnerDataTier3 = [
   },
 ];
 
-const PartnerTier3 = () => {
-  return (
-    <>
-      <div className="brand-area">
-        <div className="container">
-          <div className="brand-list">
-            {partnerDataTier3 &&
-              partnerDataTier3.map((value, i) => (
-                <div
-                  className="single-brand"
-                  data-aos="fade-in"
-                  data-aos-duration="1200"
-                  data-aos-delay={value.aosDelay}
-                  key={i}
-                >
-                  <a href={value.url} target="_blank" rel="noopener noreferrer">
-                    <img src={value.image} alt="Image" />
-                  </a>
-                </div>
-              ))}
-          </div>
+const partnerDataTier2 = [
+];
+
+const partnerDataTier1 = [
+];
+
+const PartnerTier = ({ partnerData }) => (
+  <>
+    <div className="brand-area">
+      <div className="container">
+        <div className="brand-list">
+          {partnerData.map((value, i) => (
+            <div
+              className="single-brand"
+              data-aos="fade-in"
+              data-aos-duration="1200"
+              data-aos-delay={value.aosDelay}
+              key={i}
+            >
+              <a href={value.url} target="_blank" rel="noopener noreferrer">
+                <img src={value.image} alt="Image" />
+              </a>
+            </div>
+          ))}
         </div>
       </div>
-    </>
-  );
-};
+    </div>
+  </>
+);
 
-export default PartnerTier3;
+const PartnerTier3 = () => <PartnerTier partnerData={partnerDataTier3} />;
+const PartnerTier2 = () => <PartnerTier partnerData={partnerDataTier2} />;
+const PartnerTier1 = () => <PartnerTier partnerData={partnerDataTier1} />;
+
+export { PartnerTier1, PartnerTier2, PartnerTier3 };

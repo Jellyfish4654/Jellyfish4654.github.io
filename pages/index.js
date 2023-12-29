3db053210291f8026d2cdee0e5595e06fa61edc2
Jeffrey <thebaconjr@gmail.com>
2023-12-29 18:17:43 -0500

    thonk

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
 import Navbar from "../components/Layouts/Navbar";
 import MainBanner from "../components/HomeOne/MainBanner";
 import Features from "../components/HomeOne/Features";
@@ -6,6 +8,20 @@ import Footer from "../components/Layouts/Footer";
 export default function Index() {
   return (
     <>
+      <Helmet>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZW1M1VG099"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-ZW1M1VG099');
+          `}
+        </script>
+      </Helmet>
+
       <Navbar />
 
       <MainBanner />

49053e232b5790a669c17ab9da815ef0311802d6
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2024-01-04 15:21:26 -0500

    temp fix

--- components/Layouts/Navbar.js
+++ components/Layouts/Navbar.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
-import GoogleTag from '../Common/GoogleTag';
 
 const Navbar = () => {
+  // Add active class
   const [currentPath, setCurrentPath] = useState("");
   const router = useRouter();
+  // console.log(router.asPath)
 
   useEffect(() => {
     setCurrentPath(router.asPath);
@@ -15,7 +16,6 @@ const Navbar = () => {
   const toggleNavbar = () => {
     setMenu(!menu);
   };
-
   React.useEffect(() => {
     let elementId = document.getElementById("navbar");
     document.addEventListener("scroll", () => {
@@ -36,7 +36,6 @@ const Navbar = () => {
 
   return (
     <>
-      <GoogleTag />
       <div id="navbar" className="navbar-area fixed-top">
         <nav className="navbar navbar-expand-md navbar-light">
           <div className="container">
@@ -44,6 +43,7 @@ const Navbar = () => {
               <img src="/images/white-logo.png" alt="logo" />
             </Link>
 
+            {/* Toggle navigation */}
             <button
               onClick={toggleNavbar}
               className={classTwo}
@@ -61,8 +61,72 @@ const Navbar = () => {
 
             <div className={classOne} id="navbarSupportedContent">
               <ul className="navbar-nav m-auto">
-                {/* ... navigation items ... */}
+                <li className="nav-item">
+                  <Link
+                    href="/"
+                    className={`nav-link ${currentPath == "/" && "active"}`}
+                  >
+                    Home 
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    href="/about-us/"
+                    className={`nav-link ${
+                      currentPath == "/about-us/" && "active"
+                    }`}
+                  >
+                    Our Team
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    href="/sponsors/"
+                    className={`nav-link ${
+                      currentPath == "/sponsors/" && "active"
+                    }`}
+                  >
+                    Sponsors
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    href="/juniorjellies/"
+                    className={`nav-link ${
+                      currentPath == "/juniorjellies/" && "active"
+                    }`}
+                  >
+                    Junior Jellies 
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    href="/news/"
+                    className={`nav-link ${
+                      currentPath == "/news/" && "active"
+                    }`}
+                  >
+                    News
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    href="mailto:bbhsrobotics4654@gmail.com"
+                    className={`nav-link ${
+                      currentPath == "email" && "active"
+                    }`}
+                  >
+                    Contact
+                  </Link>
+                </li>
+
               </ul>
+
             </div>
           </div>
         </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Navbar from "../components/Layouts/Navbar";
 import MainBanner from "../components/HomeOne/MainBanner";
 import Features from "../components/HomeOne/Features";
@@ -7,6 +8,8 @@ import Footer from "../components/Layouts/Footer";
 export default function Index() {
   return (
     <>
+      <GoogleTag />
+
       <Navbar />
 
       <MainBanner />

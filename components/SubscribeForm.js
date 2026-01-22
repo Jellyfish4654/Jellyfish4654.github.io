45a0f99979b71c141ceebd92c4b9cda5696fda9f
jocelyn zheng <69088223+happired@users.noreply.github.com>
2026-01-22 17:22:30 -0500

    dfs

--- components/SubscribeForm.js
+++ components/SubscribeForm.js
@@ -1,62 +1,26 @@
-import React from 'react'
-import {useMailchimp, Status} from 'hooks'
+import MailchimpSubscribe from "react-mailchimp-subscribe"
 
-const SubscribeForm = () => {
-  const [form, setForm] = React.useState({firstName: '', email: ''})
-  const {subscribe, status, error, value} = useMailchimp("https://jellyfishbbhs.us6.list-manage.com/subscribe/post?u=10a9db05c9189892653f2aad0&amp;id=a7f291a03b&amp;f_id=00e357e5f0" )
+const url = "https://jellyfishbbhs.us6.list-manage.com/subscribe/post?u=10a9db05c9189892653f2aad0&amp;id=a7f291a03b&amp;f_id=00e357e5f0";
 
-  const handleInputChange = event => {
-    const target = event.target
-    const name = target.name
-    const value = target.value
+// simplest form (only email)
+const SimpleForm = () => <MailchimpSubscribe url={url}/>
 
-    setForm(form => ({
-      ...form,
-      [name]: value,
-    }))
-  }
-
-  const handleSubmit = () => {
-    // We are performing some simple validation here.
-    // I highly recommend checking a fully-fledged solution
-    // for forms like https://react-hook-form.com/
-    if (form.firstName === '' || form.email === '') return
-
-    subscribe({
-      FNAME: form.firstName,
-      EMAIL: form.email,
-    })
-  }
-
-  if (status === Status.loading) {
-    return <Loading />
-  }
-
-  if (status === Status.error) {
-    return <Error error={error}/>
-  }
-
-  if (value.includes('Already subscribed')) {
-    return <Success subscribed />
-  }
-
-  if (value) {
-    return <Success />
-  }
-
+// use the render prop and your custom form
+const CustomForm = () => {
   return (
-    <form>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        id="firstName"
-        value={form.firstName}
-        onChange={handleInputChange}
-      />
+  <MailchimpSubscribe
+    url={url}
+    render={({ subscribe, status, message }) => (
+      <div>
+        <SimpleForm onSubmitted={formData => subscribe(formData)} />
+        {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
+        {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{__html: message}}/>}
+        {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
+      </div>
+    )}
+  />
+    )
 
-      <label htmlFor="email">Email</label>
-      <input id="email" value={form.email} onChange={handleInputChange} />
-
-      <button onClick={handleSubmit}>Subscribe 📨</button>
-    </form>
-  )
 }
+
+export default CustomForm;

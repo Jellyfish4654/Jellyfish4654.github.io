f8407492c7d43643f2c224ae41eb7904b57e6c7c
noahsausen <noahsausen@gmail.com>
2026-07-24 22:06:35 -0400

    correct way to disable page
    oops

--- pages/team.js
+++ pages/team.js
@@ -4,21 +4,28 @@ import PageBanner from "../components/Common/PageBanner";
 import TeamCard from "../components/Team/TeamCard";
 import Footer from "../components/Layouts/Footer";
 
-// export default function Team() {
-//   return (
-//     <>
-//       <Navbar />
-//
-//       <PageBanner
-//         pageTitle="Team"
-//         homePageUrl="/"
-//         homePageText="Home"
-//         activePageText="Team"
-//       />
-//
-//       <TeamCard />
-//
-//       <Footer />
-//     </>
-//   );
-// }
+// Disable page
+export async function getServerSideProps() {
+    return {
+        notFound: true
+    };
+}
+
+export default function Team() {
+  return (
+    <>
+      <Navbar />
+
+      <PageBanner
+        pageTitle="Team"
+        homePageUrl="/"
+        homePageText="Home"
+        activePageText="Team"
+      />
+
+      <TeamCard />
+
+      <Footer />
+    </>
+  );
+}

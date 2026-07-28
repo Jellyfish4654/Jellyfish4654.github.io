5d5292df929ed58c4ca6efbf4d3d562fd2e9ba23
noahsausen <noahsausen@gmail.com>
2026-07-28 13:39:29 -0400

    fix about paragraph more

--- components/OurTeam/About.js
+++ components/OurTeam/About.js
@@ -19,9 +19,10 @@ const About = () => {
                 <h2>
                   What We Do
                 </h2>
-                <p>FIRST Tech Challenge team 4654 Jellyfish was founded in 2010 and we are proud to represent our school in the FIRST Tech Challenge.
-                  We compete in the New York - Excelsior Region, covering all of NY State except for NYC and Long Island.
-                  Our goal is to inspire a love of STEM in our members and make a positive impact in our community.</p>
+                <p>FIRST Tech Challenge team 4654 Jellyfish was founded in 2010, and we are proud to represent our school
+                  at regional and international competitions. We compete in the New York Excelsior Region, covering
+                  all of NY State except for NYC and Long Island. Our goal is to inspire a love of STEM in our members
+                  and make a positive impact in our community.</p>
 
                 <p>The FIRST Tech Challenge is a global robotics competition that challenges teams to design,
                   build, and program robots they drive in 2v2 alliance matches. By participating,
